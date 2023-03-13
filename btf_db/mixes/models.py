84ecf147b2adfb90a1bf02910773acaadd22d8e0
Sam Ridding <sam@sams-air.home>
2023-03-13 20:53:05 +0000

    Add mixes model

--- btf_db/mixes/models.py
+++ btf_db/mixes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Mix(models.Model):
+
+    title = models.CharField(max_length=200, blank=False)
+    image = models.ImageField(
+        upload_to='images/', blank=False
+    )
+    content = models.TextField(blank=False)
+    audio = models.URLField(blank=True)
+    draft = models.BooleanField()
+    posted_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-posted_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
